4e305207f8d2e4ad04185766fc9f4e66218fba40
SzewczakR <37047156+SzewczakR@users.noreply.github.com>
2018-03-04 18:42:52 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] kraje = {"USA", "usa", "UK", "uk", "Poland" };
+            var kr = kraje.Distinct(StringComparer.OrdinalIgnoreCase);
+
+            foreach(var vvv in kr)
+            {
+                Console.WriteLine(vvv);
+            }
+        }
+    }
+}
